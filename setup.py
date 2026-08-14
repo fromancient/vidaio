568b6ffeab64af22555eab238c1426ea55f5fa3e
cqu3la <ibwdkgz7778@outlook.com>
2026-08-14 12:15:04 -0700

    Update setup.py
    Co-Authored-By: fromancient <55820888+fromancient@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import re
 import os
 import codecs
-from os import path
-from io import open
-from setuptools import setup, find_packages
 
 def read_requirements(path):
     with open(path, "r") as f:
